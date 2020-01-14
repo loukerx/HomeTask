36a6a56fbde34b4e8a6ccfe20a3d285cfe964ed6
Yin Hua <yin.hua@xinja.com.au>
2020-01-14 22:58:09 +1100

    Initial Commit

---
+++ HomeTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeTask
+//
+//  Created by Yin Hua on 14/1/20.
+//  Copyright © 2020 Yin Hua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
